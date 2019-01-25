5c6c8701453dfa3e5e19353acb1a0af830aba5c3
David Jensen <david_jensen3@hotmail.com>
2019-01-26 00:16:39 +0100

    Helicopter model ish

---
+++ Assets/Hook.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hook : MonoBehaviour {
+    //private Rigidbody2D pickedUpItem;
+    //public Collider2D collider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //collider = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(Input.GetKeyDown("Space")) {
+        //    if (pickedUpItem == null) {
+        //        var collisions = Physics2D.OverlapCollider(collider, new ContactFilter2D());
+        //        if (collisions.any()) {
+        //            pickedUpItem = collisions[0];
+        //            pickedUpItem.isKinematic = true;
+        //        }
+        //    }
+        //    else {
+        //        pickedUpItem.isKinematic = false;
+        //        pickedUpItem = null;
+        //    }
+        //}
+    }
+
+
+}
