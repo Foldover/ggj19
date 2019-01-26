7b4f05ea4764b238e708da07ab6ff5ef158a1c24
xarvh <francesco.orsenigo@gmail.com>
2019-01-26 11:56:09 +0100

    Add blocks spawner

---
+++ Assets/__Scripts/BlockSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BlockSpawner : MonoBehaviour
+{
+    public static GameObject[] AvailableBlocks;
+
+
+    void Start()
+    {
+         AvailableBlocks = Resources.LoadAll<GameObject>("House Building Blocks");
+         print("Loaded " + AvailableBlocks.Length + " building blocks");
+    }
+
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "House Building Block")
+        {
+          GameObject buildingBlockPrefab = AvailableBlocks[Random.Range(0, AvailableBlocks.Length)];
+          Instantiate(buildingBlockPrefab, this.transform.position, Quaternion.identity);
+        }
+    }
+}
