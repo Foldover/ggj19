6ce79d457e8667245164c11e2f5884b7ce0717ae
xarvh <francesco.orsenigo@gmail.com>
2019-01-26 13:24:04 +0100

    Sticky block WIP

---
+++ Assets/StickyBlock.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class StickyBlock : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+      print(collision.gameObject.tag + " - " + gameObject.tag);
+      if (collision.gameObject.tag == gameObject.tag)
+      {
+        print("collide!");
+      }
+
+
+
+    }
+}
