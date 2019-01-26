8a5f0e90494bde913472831cd81aab99316722b2
Pablo Sorribes Bernhard <pablo.sorribes.bernhard@gmail.com>
2019-01-26 20:35:11 +0100

    Triangle has material now

--- Assets/Triangle.cs
+++ Assets/Triangle.cs
@@ -1,35 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Triangle : MonoBehaviour
 {
+	private Mesh mesh;
+	private Vector3[] vertices;
+	private int[] triangles;
+	public Material material;
 
-    Mesh mesh;
-    Vector3[] vertices;
-    int[] triangles;
-    // Start is called before the first frame update
-    void Start()
-    {
-        gameObject.AddComponent<MeshFilter>();
-        gameObject.AddComponent<MeshRenderer>();
-        mesh = new Mesh();
-        vertices = new[]
-        {
-            new Vector3(0, 0, 0),
-            new Vector3(0, 1, 0),
-            new Vector3(1, 0, 0),
-        };
+	// Start is called before the first frame update
+	private void Start()
+	{
+		gameObject.AddComponent<MeshFilter>();
+		gameObject.AddComponent<MeshRenderer>();
+		mesh = new Mesh();
+		vertices = new[]
+		{
+			new Vector3(0, 0, 0),
+			new Vector3(0, 1, 0),
+			new Vector3(1, 0, 0),
+		};
 
-        mesh.vertices = vertices;
-        GetComponent<MeshFilter>().mesh=mesh;
-        triangles = new[] {0, 1, 2};
-        mesh.triangles = triangles;
-    }
+		mesh.vertices = vertices;
+		GetComponent<MeshFilter>().mesh = mesh;
+		triangles = new[] { 0, 1, 2 };
+		mesh.triangles = triangles;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+		gameObject.GetComponent<MeshRenderer>().material = material;
+	}
 }
--- Assets/__Scripts/Audio/HelicopterAudio.cs
+++ Assets/__Scripts/Audio/HelicopterAudio.cs
@@ -33,26 +33,7 @@ namespace Audio
 		private void Update()
 		{
 			var velocityMagnitude = rigidbod2D.velocity.magnitude;
-			var fmodEventParameterValue = VelocityToFMODEventParameterTransform(velocityMagnitude);
-
-			Debug.Log(fmodEventParameterValue);
-
-			if (ShouldUpdateParameterValue(fmodEventParameterValue))
-			{
-				helicopterSound.SetParameter("velocity", fmodEventParameterValue); //TODO: get velocity from the rigidbody pls.
-			}
-		}
-
-		private float VelocityToFMODEventParameterTransform(float velocityMagnitude)
-		{
-			return velocityMagnitude;
-		}
-
-		private bool ShouldUpdateParameterValue(float newFmodEventParameterValue)
-		{
-			var minusHysteresis = previousFmodEventParameterValue - hysteresis;
-			var plusHysteresis = previousFmodEventParameterValue + hysteresis;
-			return newFmodEventParameterValue < minusHysteresis || newFmodEventParameterValue > plusHysteresis;
+			helicopterSound.SetParameter("velocity", velocityMagnitude);
 		}
 	}
 }
