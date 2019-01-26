bc56a20c4fb7b67c9b7c818bd1cad1478c184d66
xarvh <francesco.orsenigo@gmail.com>
2019-01-26 14:05:42 +0100

    Add color-based sticky mechanic

--- Assets/__Scripts/BlockSpawner.cs
+++ Assets/__Scripts/BlockSpawner.cs
@@ -8,6 +8,13 @@ public class BlockSpawner : MonoBehaviour
     public static GameObject[] AvailableBlocks;
 
 
+    public static string[] Colors = {
+      "Red",
+      "Blue",
+      "Green"
+    };
+
+
     void Start()
     {
          AvailableBlocks = Resources.LoadAll<GameObject>("House Building Blocks");
@@ -19,8 +26,11 @@ public class BlockSpawner : MonoBehaviour
     {
         if (collider.gameObject.tag == "House Building Block")
         {
+          print("Stick!");
           GameObject buildingBlockPrefab = AvailableBlocks[Random.Range(0, AvailableBlocks.Length)];
-          Instantiate(buildingBlockPrefab, this.transform.position, Quaternion.identity);
+          GameObject block = Instantiate(buildingBlockPrefab, this.transform.position, Quaternion.identity);
+          StickyBlock sticky = block.AddComponent<StickyBlock>();
+          sticky.color = Colors[Random.Range(0, Colors.Length)];
         }
     }
 }
---
+++ Assets/__Scripts/StickyBlock.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class StickyBlock : MonoBehaviour
+{
+    public string color;
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        StickyBlock otherBlock = collision.gameObject.GetComponent<StickyBlock>();
+        if (otherBlock && otherBlock.color == color)
+        {
+            FixedJoint2D joint = gameObject.AddComponent<FixedJoint2D>();
+            joint.connectedBody = collision.rigidbody;
+        }
+    }
+}
