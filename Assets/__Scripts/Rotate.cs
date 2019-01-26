137e42cfc85e5d1cc83b73d3af0f315093bd2a6e
xarvh <francesco.orsenigo@gmail.com>
2019-01-26 16:04:22 +0100

    Helicopter has rotating propellers

---
+++ Assets/__Scripts/Rotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float SpeedZ = 100;
+    public float SpeedY = 0;
+
+    void Update()
+    {
+      transform.Rotate((Vector3.up * SpeedY + Vector3.forward * SpeedZ) * Time.deltaTime);
+    }
+}
