b2bea868be299374c84add26748610c1099357c8
Pablo Sorribes Bernhard <pablo.sorribes.bernhard@gmail.com>
2019-01-26 20:38:38 +0100

    cleanup of Helicopter.cs

--- Assets/__Scripts/Helicopter.cs
+++ Assets/__Scripts/Helicopter.cs
@@ -1,32 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Helicopter : MonoBehaviour
 {
-    public float Speed = 0.5f; 
-    public Rigidbody2D body;
+	public float speed = 0.5f;
+	private Rigidbody2D body;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        body = gameObject.GetComponent<Rigidbody2D>();
+	private void Start()
+	{
+		body = gameObject.GetComponent<Rigidbody2D>();
 	}
 
-	// Update is called once per frame
-	void Update()
-    {
-        if (Input.GetKey ("d")) {
-            body.AddForce(Vector3.right * Speed);
-        } 
-        if (Input.GetKey ("a")) {
-            body.AddForce(Vector3.left * Speed);
-        } 
-        if (Input.GetKey ("w")) {
-            body.AddForce(Vector3.up * Speed);
-        } 
-        if (Input.GetKey ("s")) {
-            body.AddForce(Vector3.down * Speed);
-        }
-    }
+	private void Update()
+	{
+		if (Input.GetKey("d"))
+		{
+			body.AddForce(Vector3.right * speed);
+		}
+		if (Input.GetKey("a"))
+		{
+			body.AddForce(Vector3.left * speed);
+		}
+		if (Input.GetKey("w"))
+		{
+			body.AddForce(Vector3.up * speed);
+		}
+		if (Input.GetKey("s"))
+		{
+			body.AddForce(Vector3.down * speed);
+		}
+	}
 }
