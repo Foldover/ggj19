d698585d90d02bb6cf2470486b4084ccc8c1f163
Jacob <jacob.romano@metry.io>
2019-01-26 22:32:10 +0100

    made a score indicator

---
+++ Assets/__Scripts/ScoreGUI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreGUI : MonoBehaviour
+{
+    [SerializeField] private Text scoreText;
+    [SerializeField] private HouseComparer houseComparer;
+
+    private void Awake()
+    {
+        scoreText.text = $"{0}%";
+    }
+
+    private void Update()
+    {
+        scoreText.text = $"{houseComparer.score}%";
+    }
+}
